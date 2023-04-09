655d3121abe3c747144ae8c722ceef0c5afd26d3
ZeroCondition <70964202+zer0condition@users.noreply.github.com>
2023-04-09 18:13:22 +0530

    Update commit.

---
+++ GDRVLoader.cpp
@@ -0,0 +1,45 @@
+#include "global.h"
+#include "binary/dropper.h"
+#include <Windows.h>
+
+const wchar_t* DriverPath = L"C:\\Windows\\System32\\Drivers\\gdrv.sys";
+
+int wmain(int argc, wchar_t** argv)
+{
+    if (argc < 2) {
+        printf("Invalid arguments. Usage: GDRVLoader.exe TargetDriver.sys\n");
+        return false;
+    }
+
+    NTSTATUS Status = STATUS_UNSUCCESSFUL;
+
+    char input[10];
+    printf("Load or unload driver?\n");
+    scanf("%s", input);
+
+    if (strcmp(input, "LOAD") == 0 || strcmp(input, "load") == 0)
+    {
+        if (DropDriverFromBytes(DriverPath))
+        {
+            // Load driver
+            Status = WindLoadDriver((PWCHAR)DriverPath, argv[1], FALSE);
+
+            if (NT_SUCCESS(Status))
+                printf("Driver loaded successfully\n");
+
+            DeleteFile((PWSTR)DriverPath);
+        }
+    }
+    else if (strcmp(input, "Unload") == 0 || strcmp(input, "unload") == 0)
+    {
+        // Unload driver
+        Status = WindUnloadDriver((PWCHAR)argv[1], 0);
+        if (NT_SUCCESS(Status))
+            printf("Driver unloaded successfully\n");
+    }
+
+    if (!NT_SUCCESS(Status))
+        printf("Error: %08X\n", Status);
+
+    return true;
+}
